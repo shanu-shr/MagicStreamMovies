ef35f4c5c99c18911a138d7f0669a945e9d7066e
abc <a>
2025-10-22 17:13:01 +0530

    add struct tags

--- Server/MagicMovieStreamServer/Models/genre.go
+++ Server/MagicMovieStreamServer/Models/genre.go
@@ -1,6 +1,6 @@
 package models
 
 type Genre struct {
-	GenreID   int
-	GenreName string
+	GenreID   int    `bson:"genre_id" json:"genre_id" validate:"required"`
+	GenreName string `bson:"genre_name" json:"genre_name" validate:"required, min=2, max=100"`
 }
--- Server/MagicMovieStreamServer/Models/movie.go
+++ Server/MagicMovieStreamServer/Models/movie.go
@@ -3,12 +3,12 @@ package models
 import "go.mongodb.org/mongo-driver/v2/bson"
 
 type Movie struct {
-	ID          bson.ObjectID
-	ImdbID      string
-	Title       string
-	PosterPath  string
-	YouTubeId   string
-	Genre       []Genre
-	AdminReview string
-	Ranking     []Ranking
+	ID          bson.ObjectID `bson:"_id" json:"_id"`
+	ImdbID      string        `bson:"imdb_id" json:"imdb_id" validate:"required"`
+	Title       string        `bson:"title" json:"title" validate:"required, min=2, max=500"`
+	PosterPath  string        `bson:"poster_path" json:"poster_path" validate:"required, url"`
+	YouTubeId   string        `bson:"youtube_id" json:"youtube_id" validate:"required"`
+	Genre       []Genre       `bson:"genre" json:"genre" validate:"required, dive"`
+	AdminReview string        `bson:"admin_review" json:"admin_review" validate:"required"`
+	Ranking     []Ranking     `bson:"ranking" json:"ranking" validate:"required,dive"`
 }
